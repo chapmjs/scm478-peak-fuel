fadffe5de30ed89674df4fe820a57cc05aeda431
John Chapman <john.chapman@systemsresearch.linkpc.net>
2026-04-15 17:38:33 -0600

    Include sales data in load_data function

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def load_data():
     products = pd.read_csv("Products_Pricing.csv")
     ingredients = pd.read_csv("Ingredient_Catalog.csv")
     sales = pd.read_csv("Sales_Log.csv", parse_dates=["Date"])
-    return products, ingredients
+    return products, ingredients, sales
 
 products_df, ingredients_df, sales_df = load_data()
 
